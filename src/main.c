054b51fe1fc027f44dd30d6c71ab990a602a7813
unknown <kenneth.restauro@g.msuiit.edu.ph>
2026-09-06 19:15:03 +0800

    Configured Wokwi Simulation, Modified flash size to 2MB

--- src/main.c
+++ src/main.c
@@ -1,1 +1,25 @@
-void app_main() {}
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <esp_log.h>
+
+const char *SERIALMONITOR_TAG = "MAIN APP";
+const TickType_t delay = 500 / portTICK_PERIOD_MS;
+
+void app_main() {
+    ESP_LOGI(SERIALMONITOR_TAG, "\nBCA152 FreeRTOS Multi-sensor\nSystem Starting...");
+
+    while(true) {
+        vTaskDelay(delay);
+    }
+}
+
+
+/* Note:
+    Blocking (RTOS type of Blocking)
+    vTaskDelay(delay);
+    Pauses and checks other queued tasks. Remains blocked even if there is no other tasks
+
+    Non-blocking
+    taskYIELD();
+    Pauses and checks other queued tasks. Resumes immediately if no other tasks are found/ready
+*/
