170051e87c3ca19f8ba1d71a8b08e213601b9e0e
unknown <kenneth.restauro@g.msuiit.edu.ph>
2026-09-17 13:20:21 +0800

    Added Buzzer Pin Setup function

--- include/myBUZZER_Setup.hpp
+++ include/myBUZZER_Setup.hpp
@@ -1,12 +1,19 @@
 #ifndef MYBUZZER_SETUP_HPP
 #define MYBUZZER_SETUP_HPP
 
+// Libraries
+extern "C" {
+    #include <GPIO_Setup.h>
+}
+
 enum class AlarmState {
     NORMAL,
     LOW_TEMPERATURE,
     HIGH_TEMPERATURE
 };
 
+// Function Declarations
+void buzzerPin_Setup(uint64_t pin_mask);
 AlarmState evaluateTemperature(float temperature);
 
 #endif
--- src/myBUZZER_Setup.cpp
+++ src/myBUZZER_Setup.cpp
@@ -1,6 +1,11 @@
 #include <myBUZZER_Setup.hpp>
 
+static gpio_config_t BUZZER_Pin;
 static AlarmState currentTemperatureMode;
+
+void buzzerPin_Setup(uint64_t pin_mask) {
+    gpio_init(&BUZZER_Pin, pin_mask, GPIO_MODE_INPUT, GPIO_PULLUP_DISABLE, GPIO_PULLDOWN_DISABLE, GPIO_INTR_DISABLE);
+}
 
 AlarmState evaluateTemperature(float temperature) {
     if(temperature < 10.0f) {
