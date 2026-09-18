0d3fef028de55bed504a6d82c91d173d6399b6f3
unknown <kenneth.restauro@g.msuiit.edu.ph>
2026-09-18 14:00:56 +0800

    Fixed BUZZER Pin Config, LEDC Config & Channel Appropriately

--- src/myBUZZER_Setup.cpp
+++ src/myBUZZER_Setup.cpp
@@ -7,13 +7,13 @@ static uint8_t pin_num;
 void buzzerPin_Setup(uint64_t pin_mask) {
     #pragma GCC diagnostic push
     #pragma GCC diagnostic ignored "-Wmissing-field-initializers"
-    gpio_init(&BUZZER_Pin, pin_mask, GPIO_MODE_INPUT, GPIO_PULLUP_DISABLE, GPIO_PULLDOWN_DISABLE, GPIO_INTR_DISABLE);
+    gpio_init(&BUZZER_Pin, pin_mask, GPIO_MODE_OUTPUT, GPIO_PULLUP_DISABLE, GPIO_PULLDOWN_DISABLE, GPIO_INTR_DISABLE);
     pin_num = __builtin_ffsll(pin_mask) - 1; // Convert bit_mask to GPIO_NUM
     ledc_timer_config_t timer_config = {
         .speed_mode = LEDC_LOW_SPEED_MODE,
         .duty_resolution = LEDC_TIMER_8_BIT,
         .timer_num = LEDC_TIMER_0,
-        .freq_hz = 1000,
+        .freq_hz = 2000,
         .clk_cfg = LEDC_AUTO_CLK
     };
     ledc_timer_config(&timer_config);
@@ -34,9 +34,9 @@ void buzzerPin_Setup(uint64_t pin_mask) {
 }
 
 AlarmState evaluateTemperature(float temperature) {
-    if(temperature < 10.0f) {
+    if(temperature <= 18.0f) {
         return currentTemperatureMode = AlarmState::LOW_TEMPERATURE;
-    } else if(temperature > 35.0f) {
+    } else if(temperature >= 30.0f) {
         return currentTemperatureMode = AlarmState::HIGH_TEMPERATURE;
     }
     return currentTemperatureMode = AlarmState::NORMAL;
