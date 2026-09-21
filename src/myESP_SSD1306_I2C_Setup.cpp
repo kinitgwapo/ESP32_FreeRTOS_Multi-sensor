5791e62bf88cf71cbfca92628dd74fd3eb6f4cf4
unknown <kenneth.restauro@g.msuiit.edu.ph>
2026-09-21 13:40:56 +0800

    Removed PIR Source & Header File Clutter, Updated Display source file lightLevel to Percentage

--- src/myESP_SSD1306_I2C_Setup.cpp
+++ src/myESP_SSD1306_I2C_Setup.cpp
@@ -48,7 +48,7 @@ void display_update_screen(SensorData *data, uint8_t currentMode, bool isSystemA
                 break;
             case DisplayMode::LIGHT:
                 ssd1306_draw_text(display_handle, 0, 20, "LIGHT", true);
-                snprintf(temporaryText, sizeof(temporaryText), "%d", data->lightLevel);
+                snprintf(temporaryText, sizeof(temporaryText), "%d%%", data->lightLevel);
                 break;
             case DisplayMode::MOTION:
                 ssd1306_draw_text(display_handle, 0, 20, "MOTION", true);
--- src/myPIR_Setup.c
+++ src/myPIR_Setup.c
@@ -7,6 +7,40 @@ void pirPin_Setup(uint64_t pin_mask) {
     gpio_init(&PIR_Pin, pin_mask, GPIO_MODE_INPUT, GPIO_PULLUP_DISABLE, GPIO_PULLDOWN_DISABLE, GPIO_INTR_DISABLE);
 }
 
+void pir_evaluateMotion(bool status, bool *prevstatus) {
+    if(status) {
+        xEventGroupSetBits(systemEventGroup, EVENT_MOTION);
+        if(*prevstatus == false) {
+            if(xSemaphoreTake(serialMutex, portMAX_DELAY)) {
+                ESP_LOGI(PIR_TAG, "Motion Sensor Triggered!");
+                xSemaphoreGive(serialMutex);
+            }
+        }
+    } else {
+        xEventGroupClearBits(systemEventGroup, EVENT_MOTION);
+    }
+
+    *prevstatus = status;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 void pir_InActiveTOActive(bool *status, bool *prevstatus) {
     if(*status == true && *prevstatus == false) {
         if(!(xEventGroupGetBits(systemEventGroup) & EVENT_ACTIVE)) {
@@ -45,3 +79,4 @@ void pir_ActiveTOInActive(TickType_t *lastMotionTick) {
         }
     }
 }
+*/
